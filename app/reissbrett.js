8c718d66e1f19270a011a938da1697aff455cc61
Oliver Roick <oliver.roick@gmail.com>
2015-06-22 20:00:11 +0100

    Design

--- app/reissbrett.js
+++ app/reissbrett.js
@@ -1,11 +1,24 @@
 (function (global) {
     'use strict';
 
-    var geometryTools = global.control.geometryTools,
-        map =           global.control.map;
+    var map = global.control.map;
 
     function Reissbrett() {
-        geometryTools.registerListener('tools:newClicked', map.activateDraw);
+        map.registerListener('draw:conflict:selfintersect', handleSelfIntersect);
+        map.registerListener('draw:conflict:intersect', handleIntersect);
+        map.registerListener('draw:start', handleDrawStart);
+    }
+
+    function handleDrawStart() {
+        console.log('draw:start');
+    }
+
+    function handleSelfIntersect() {
+        console.log('draw:conflict:selfintersect');
+    }
+
+    function handleIntersect() {
+        console.log('draw:conflict:intersect');
     }
 
     global.app = new Reissbrett();
