1dd87a869d0c1a8195621cb20a6fdb5cd61d6195
Oliver Roick <oliver.roick@gmail.com>
2015-06-22 20:00:22 +0100

    Check self intersect

--- app/control/map.js
+++ app/control/map.js
@@ -2,9 +2,16 @@
     'use strict';
 
     var map,
+        // layers
         featuresDrawn = new L.LayerGroup(),
         featuresCommitted = new L.LayerGroup(),
-        allowIntersect = false;
+
+        // rules
+        allowIntersect = false,
+        allowSelfIntersect = false,
+
+        // UI
+        newBtn;
 
         var styles = {
             COMMITTED: {
@@ -26,7 +33,11 @@
                 fillOpacity: 0.4
             },
             INTERSECTION: {
-                stroke: false,
+                stroke: true,
+                color: '#CF000F',
+                weight: 2,
+                opacity: 1,
+                dashArray: '5, 10',
                 fill: true,
                 fillColor: '#CF000F',
                 fillOpacity: 0.8
@@ -34,9 +45,8 @@
         };
 
     function Map (){
-        L.mapbox.accessToken = SETTINGS.MAPBOX.pk;
         var mapboxTiles = L.tileLayer(
-            'https://{s}.tiles.mapbox.com/v4/examples.map-i87786ca/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
+            'https://{s}.tiles.mapbox.com/v4/examples.map-i87786ca/{z}/{x}/{y}.png?access_token=' + SETTINGS.MAPBOX.pk, {
                 attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
             }
         );
@@ -44,21 +54,42 @@
             .setView([51.50719323477933, -0.12754440307617188], 9);
 
         map.addLayer(mapboxTiles);
-        map.addLayer(featuresDrawn);
         map.addLayer(featuresCommitted);
+        map.addLayer(featuresDrawn);
 
         map.on('editable:drawing:commit', checkIntersect);
+        map.on('editable:drawing:clicked', checkSelfIntersect);
+
+        newBtn = document.getElementById('new-btn');
+        newBtn.onclick = activateDraw;
     }
+    Map.prototype = new EventEmitter();
+    Map.constructor = Map;
     global.map = new Map();
 
     function activateDraw () {
+        global.map.emitEvent('draw:start');
         map.editTools.startPolygon();
     }
     Map.prototype.activateDraw = activateDraw;
 
+    function checkSelfIntersect(event) {
+        var drawnLayer = event.layer;
+
+        if (!allowSelfIntersect) {
+            var kinks = turf.kinks(drawnLayer.toGeoJSON());
+            if (kinks.intersections.features.length) {
+                map.editTools._drawingEditor.pop();
+                global.map.emitEvent('draw:conflict:selfintersect');
+            }
+        }
+    }
+
     function checkIntersect(layer) {
         var drawnLayer = layer.layer,
             intersections = [];
+
+        drawnLayer.disableEdit();
 
         if (!allowIntersect) {
             featuresCommitted.eachLayer(function(existingLayer) {
@@ -76,6 +107,7 @@
             featuresCommitted.addLayer(drawnLayer);
         } else {
             drawnLayer.setStyle(styles.CONFLICT);
+            global.map.emitEvent('draw:conflict:intersect');
         }
     }
 
